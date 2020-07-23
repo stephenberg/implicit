0cfa71e317633c5d2a4f90c304062c79ac49435a
Stephen Berg <saberg2@wisc.edu>
2020-07-23 14:23:19 -0400

    start programming derivatives

--- src/inversionFunctions.h
+++ src/inversionFunctions.h
@@ -235,18 +235,3 @@ Eigen::VectorXd invert_Irregular(int rows,
 }
 
 
-//discrete sine transformation implementation
-//primarily for testing purposes
-//[[Rcpp::export]]
-Eigen::MatrixXd dst1(Eigen::MatrixXd U){
-  int rows=U.rows();
-  int cols=U.cols();
-  Eigen::MatrixXd V(rows,cols);
-  fftPlan p(rows,
-            cols,
-            U.data(),
-            V.data(),
-            U.data());
-  p.dst_1();
-  return(V);
-}
--- typedefs.h
+++
@@ -1,5 +0,0 @@
-#include <Rcpp.h>
-#include <RcppEigen.h>
-
-typedef Eigen::VectorXd vec;
-typedef Eigen::MatrixXd map;
