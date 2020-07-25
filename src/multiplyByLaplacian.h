46b7af2c8d5a535f77849e06abe22def1fa7072b
Stephen Berg <saberg2@wisc.edu>
2020-07-24 21:39:51 -0400

    update laplacian multiply

--- src/multiplyByLaplacian.h
+++ src/multiplyByLaplacian.h
@@ -25,6 +25,7 @@ Eigen::VectorXd fick_L_f(Eigen::VectorXd& mu_,
                          Eigen::VectorXd& f_,
                          int rows,
                          int cols,
+                         bool dirichlet=true,
                          double lengthX=1,
                          double lengthY=1){
   Eigen::VectorXd b_;
@@ -90,8 +91,18 @@ Eigen::VectorXd fick_L_f(Eigen::VectorXd& mu_,
 Eigen::VectorXd homogeneous_L_f(Eigen::VectorXd& f_,
                                 int rows,
                                 int cols,
+                                bool dirichlet=true,
                                 double lengthX=1,
                                 double lengthY=1){
+  
+  double boundaryMultiplier;
+  if (dirichlet){
+    boundaryMultiplier=2.0;
+  }
+  else{
+    boundaryMultiplier=1.0;
+  }
+  
   Eigen::VectorXd b_;
   b_.setZero(rows*cols);
   
@@ -108,10 +119,10 @@ Eigen::VectorXd homogeneous_L_f(Eigen::VectorXd& f_,
   for (int j=0;j<cols;j++){
     for (int i=0;i<rows;i++){
       if (j==0){
-        b(i,j)+=(f(i,j+1)-2*f(i,j))/std::pow(h_x,2);
+        b(i,j)+=(f(i,j+1)-boundaryMultiplier*f(i,j))/std::pow(h_x,2);
       }
       if (j==(cols-1)){
-        b(i,j)+=(f(i,j-1)-2*f(i,j))/std::pow(h_x,2);
+        b(i,j)+=(f(i,j-1)-boundaryMultiplier*f(i,j))/std::pow(h_x,2);
       }
       if ((j>0)&(j<(cols-1))){
         b(i,j)+=(f(i,j+1)+f(i,j-1)-2*f(i,j))/std::pow(h_x,2);
@@ -123,10 +134,10 @@ Eigen::VectorXd homogeneous_L_f(Eigen::VectorXd& f_,
   for (int j=0;j<cols;j++){
     for (int i=0;i<rows;i++){
       if (i==0){
-        b(i,j)+=(f(i+1,j)-2*f(i,j))/std::pow(h_y,2);
+        b(i,j)+=(f(i+1,j)-boundaryMultiplier*f(i,j))/std::pow(h_y,2);
       }
       if (i==(rows-1)){
-        b(i,j)+=(f(i-1,j)-2*f(i,j))/std::pow(h_y,2);
+        b(i,j)+=(f(i-1,j)-boundaryMultiplier*f(i,j))/std::pow(h_y,2);
       }
       if ((i>0)&(i<(rows-1))){
         b(i,j)+=(f(i+1,j)+f(i-1,j)-2*f(i,j))/std::pow(h_y,2);
@@ -141,14 +152,14 @@ Eigen::VectorXd homogeneous_L_f(Eigen::VectorXd& f_,
 Eigen::VectorXd general_Homogeneous_Lf(Eigen::VectorXd& f_,
                                        int rows,
                                        int cols,
-                                       Eigen::VectorXi& boundaryPoints_,
+                                       Eigen::VectorXi& internalPoints_,
                                        bool dirichlet=true,
                                        double lengthX=1,
                                        double lengthY=1){
   Eigen::VectorXd b_;
   b_.setZero(rows*cols);
   
-  Eigen::Map<MatrixXi> boundary(boundaryPoints_.data(),rows,cols);
+  Eigen::Map<MatrixXi> internal(internalPoints_.data(),rows,cols);
   
   double h_y,h_x;
   h_y=lengthY/(rows+1);
@@ -165,31 +176,31 @@ Eigen::VectorXd general_Homogeneous_Lf(Eigen::VectorXd& f_,
       if (j==0){
         //Dirichlet
         if (dirichlet){
-          b(i,j)+=boundary(i,j)*(boundary(i,j+1)*f(i,j+1)-2*f(i,j))/std::pow(h_x,2);
+          b(i,j)+=internal(i,j)*(internal(i,j+1)*f(i,j+1)-2*f(i,j))/std::pow(h_x,2);
         }
         //Neumann
         else{
-          b(i,j)+=boundary(i,j)*(boundary(i,j+1)*(f(i,j+1)-f(i,j)))/std::pow(h_x,2);
+          b(i,j)+=internal(i,j)*(internal(i,j+1)*(f(i,j+1)-f(i,j)))/std::pow(h_x,2);
         }
       }
       if (j==(cols-1)){
         //Dirichlet
         if (dirichlet){
-          b(i,j)+=boundary(i,j)*(boundary(i,j-1)*f(i,j-1)-2*f(i,j))/std::pow(h_x,2);
+          b(i,j)+=internal(i,j)*(internal(i,j-1)*f(i,j-1)-2*f(i,j))/std::pow(h_x,2);
         }
         //Neumann
         else{
-          b(i,j)+=boundary(i,j)*(boundary(i,j-1)*(f(i,j-1)-f(i,j)))/std::pow(h_x,2);
+          b(i,j)+=internal(i,j)*(internal(i,j-1)*(f(i,j-1)-f(i,j)))/std::pow(h_x,2);
         }
       }
       if ((j>0)&(j<(cols-1))){
         //Dirichlet
         if (dirichlet){
-          b(i,j)+=boundary(i,j)*(boundary(i,j+1)*f(i,j+1)+boundary(i,j-1)*f(i,j-1)-2*f(i,j))/std::pow(h_x,2);
+          b(i,j)+=internal(i,j)*(internal(i,j+1)*f(i,j+1)+internal(i,j-1)*f(i,j-1)-2*f(i,j))/std::pow(h_x,2);
         }
         //Neumann
         else{
-          b(i,j)+=boundary(i,j)*(boundary(i,j+1)*(f(i,j+1)-f(i,j))-boundary(i,j-1)*(f(i,j)-f(i,j-1)))/std::pow(h_x,2);
+          b(i,j)+=internal(i,j)*(internal(i,j+1)*(f(i,j+1)-f(i,j))-internal(i,j-1)*(f(i,j)-f(i,j-1)))/std::pow(h_x,2);
         }
       }
     }
@@ -201,30 +212,145 @@ Eigen::VectorXd general_Homogeneous_Lf(Eigen::VectorXd& f_,
       if (i==0){
         //Dirichlet
         if (dirichlet){
-          b(i,j)+= boundary(i, j)*(boundary(i+1,j)*f(i+1,j)-2*f(i,j))/std::pow(h_y,2);
+          b(i,j)+= internal(i, j)*(internal(i+1,j)*f(i+1,j)-2*f(i,j))/std::pow(h_y,2);
         }
         //Neumann
         else{
-          b(i,j)+=boundary(i,j)*(boundary(i+1,j)*(f(i+1,j)-f(i,j)))/std::pow(h_y,2);
+          b(i,j)+=internal(i,j)*(internal(i+1,j)*(f(i+1,j)-f(i,j)))/std::pow(h_y,2);
         }
       }
       if (i==(rows-1)){
         //Dirichlet
         if (dirichlet){
-          b(i,j)+= boundary(i, j)*(boundary(i-1,j)*f(i-1,j)-2*f(i,j))/std::pow(h_y,2);
+          b(i,j)+= internal(i, j)*(internal(i-1,j)*f(i-1,j)-2*f(i,j))/std::pow(h_y,2);
         }
         else{
-          b(i,j)+=boundary(i,j)*(boundary(i-1,j)*(f(i-1,j)-f(i,j)))/std::pow(h_y,2);
+          b(i,j)+=internal(i,j)*(internal(i-1,j)*(f(i-1,j)-f(i,j)))/std::pow(h_y,2);
         }
       }
       if ((i>0)&(i<(rows-1))){
         //Dirichlet
         if (dirichlet){
-          b(i,j)+= boundary(i, j)*(boundary(i+1,j)*f(i+1,j)+boundary(i-1,j)*f(i-1,j)-2*f(i,j))/std::pow(h_y,2);
+          b(i,j)+= internal(i, j)*(internal(i+1,j)*f(i+1,j)+internal(i-1,j)*f(i-1,j)-2*f(i,j))/std::pow(h_y,2);
         }
         //Neumann
         else{
-          b(i,j)+=boundary(i,j)*(boundary(i-1,j)*(f(i-1,j)-f(i,j))-boundary(i+1,j)*(f(i,j)-f(i+1,j)))/std::pow(h_y,2);
+          b(i,j)+=internal(i,j)*(internal(i-1,j)*(f(i-1,j)-f(i,j))-internal(i+1,j)*(f(i,j)-f(i+1,j)))/std::pow(h_y,2);
+        }
+      }
+    }
+  }
+  return b_;
+}
+
+
+//implicit multiplication by the Laplacian with homogeneous diffusion
+//[[Rcpp::export]]
+Eigen::VectorXd general_Fick_Lf(Eigen::VectorXd& mu_,
+                                Eigen::VectorXd& f_,
+                                int rows,
+                                int cols,
+                                Eigen::VectorXi& internalPoints_,
+                                bool dirichlet=true,
+                                double lengthX=1,
+                                double lengthY=1){
+  Eigen::VectorXd b_;
+  b_.setZero(rows*cols);
+  
+  
+  Eigen::Map<MatrixXi> internal(internalPoints_.data(),rows,cols);
+  
+  double h_y,h_x;
+  h_y=lengthY/(rows+1);
+  h_x=lengthX/(cols+1);
+  
+  //use value of diffusion coefficient for boundary cells
+  Map<Eigen::MatrixXd> mu(mu_.data(),rows+2,cols+2);
+  
+  //f and b implicitly defined as 0 on boundary points
+  Map<Eigen::MatrixXd> f(f_.data(),rows,cols);
+  Map<Eigen::MatrixXd> b(b_.data(),rows,cols);
+  
+  //Fickian diffusion
+  
+  //d2dx2
+  for (int j=0;j<cols;j++){
+    for (int i=0;i<rows;i++){
+      double leftMu,rightMu;
+      
+      //note: mu contains the boundary points, in particular
+      //mu(i+1,j+1) corresponds to the point f(i,j)
+      leftMu=(mu(i+1,j)+mu(i+1,j+1))/2;
+      rightMu=(mu(i+1,j+1)+mu(i+1,j+2))/2;
+      
+      if (j==0){
+        //Dirichlet
+        if (dirichlet){
+          b(i,j)+=internal(i,j)*(internal(i,j+1)*rightMu*f(i,j+1)-(leftMu+rightMu)*f(i,j))/std::pow(h_x,2);
+        }
+        //Neumann
+        else{
+          b(i,j)+=internal(i,j)*(internal(i,j+1)*rightMu*(f(i,j+1)-f(i,j)))/std::pow(h_x,2);
+        }
+      }
+      if (j==(cols-1)){
+        //Dirichlet
+        if (dirichlet){
+          b(i,j)+=internal(i,j)*(internal(i,j-1)*leftMu*f(i,j-1)-(leftMu+rightMu)*f(i,j))/std::pow(h_x,2);
+        }
+        //Neumann
+        else{
+          b(i,j)+=internal(i,j)*(internal(i,j-1)*leftMu*(f(i,j-1)-f(i,j)))/std::pow(h_x,2);
+        }
+      }
+      if ((j>0)&(j<(cols-1))){
+        //Dirichlet
+        if (dirichlet){
+          b(i,j)+=internal(i,j)*(internal(i,j+1)*rightMu*f(i,j+1)+internal(i,j-1)*leftMu*f(i,j-1)-(leftMu+rightMu)*f(i,j))/std::pow(h_x,2);
+        }
+        //Neumann
+        else{
+          b(i,j)+=internal(i,j)*(internal(i,j+1)*rightMu*(f(i,j+1)-f(i,j))-internal(i,j-1)*leftMu*(f(i,j)-f(i,j-1)))/std::pow(h_x,2);
+        }
+      }
+    }
+  }
+  
+  //d2dy2
+  for (int j=0;j<cols;j++){
+    for (int i=0;i<rows;i++){
+      
+      double upperMu,lowerMu;
+      upperMu=(mu(i,j+1)+mu(i+1,j+1))/2;
+      lowerMu=(mu(i+1,j+1)+mu(i+2,j+1))/2;
+      
+      if (i==0){
+        //Dirichlet
+        if (dirichlet){
+          b(i,j)+= internal(i, j)*(internal(i+1,j)*lowerMu*f(i+1,j)-(lowerMu+upperMu)*f(i,j))/std::pow(h_y,2);
+        }
+        //Neumann
+        else{
+          b(i,j)+=internal(i,j)*(internal(i+1,j)*lowerMu*(f(i+1,j)-f(i,j)))/std::pow(h_y,2);
+        }
+      }
+      if (i==(rows-1)){
+        //Dirichlet
+        if (dirichlet){
+          b(i,j)+= internal(i, j)*(internal(i-1,j)*upperMu*f(i-1,j)-(lowerMu+upperMu)*f(i,j))/std::pow(h_y,2);
+        }
+        else{
+          b(i,j)+=internal(i,j)*(internal(i-1,j)*upperMu*(f(i-1,j)-f(i,j)))/std::pow(h_y,2);
+        }
+      }
+      if ((i>0)&(i<(rows-1))){
+        //Dirichlet
+        if (dirichlet){
+          b(i,j)+= internal(i, j)*(internal(i+1,j)*lowerMu*f(i+1,j)+internal(i-1,j)*upperMu*f(i-1,j)-(lowerMu+upperMu)*f(i,j))/std::pow(h_y,2);
+        }
+        //Neumann
+        else{
+          b(i,j)+=internal(i,j)*(internal(i-1,j)*upperMu*(f(i-1,j)-f(i,j))-internal(i+1,j)*lowerMu*(f(i,j)-f(i+1,j)))/std::pow(h_y,2);
         }
       }
     }
